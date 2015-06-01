ebd0752a09097860f6edbe3197a3a9496aa8700e
John Wu <webmaster@leapoahead.com>
2015-06-01 10:38:24 +0800

    disqus

--- themes/powerful.parts/scripts/archive.js
+++
@@ -1,8 +0,0 @@
-var pagination = require('hexo-pagination');
-hexo.extend.generator.register('archive', function(locals){
-  return pagination('index.html', locals.posts, {
-    perPage: 1,
-    layout: ['index'],
-    data: {}
-  });
-});
